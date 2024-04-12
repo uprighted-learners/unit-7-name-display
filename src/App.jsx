0775a1c8c460466a2aea1674274e608101bb7b3f
Ben Anderson <andersjbe@gmail.com>
2024-04-12 15:58:59 -0400

    Completed Solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [firstName, setFirstName] = useState("");
+  const [middleName, setMiddleName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const fullName = `${firstName} ${middleName} ${lastName}`;
+
+  const message = `Welcome to React ${fullName}!`;
   return (
-    <>App</>
+    <div>
+      <h1>{message}</h1>;
+      <form>
+        <input
+          type="text"
+          placeholder="first name"
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="middle name"
+          onChange={(e) => setMiddleName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="last name"
+          onChange={(e) => setLastName(e.target.value)}
+        />
+      </form>
+    </div>
   );
 }
 
